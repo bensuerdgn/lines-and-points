12a7a75cd4c3821b8ef1b0ea3a5aeb4f874e01ab
Bensu Erdoğan <bensuerdogan58@gmail.com>
2020-11-06 16:36:21 +0300

    değişiklik yapıldı

--- src/square/Square.java
+++ src/square/Square.java
@@ -10,9 +10,14 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 interface Variable {
 
@@ -21,25 +26,51 @@ interface Variable {
     public int y = r.nextInt((600 - 200) + 1) + 200;
 }
 
-class Surface extends JPanel implements Variable {
+class Surface extends JPanel implements Variable, ActionListener {
+
+    private final int DELAY = 150;
+    private Timer timer;
+
+    public Surface() {
+
+        initTimer();
+    }
+
+    private void initTimer() {
+
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public Timer getTimer() {
+
+        return timer;
+    }
 
     private void doDrawing(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        
+
         Random r = new Random();
-        
-        float[] dash={r.nextFloat(), r.nextFloat(), r.nextFloat()};
+        float[] dash = {r.nextFloat(), r.nextFloat(), r.nextFloat()};
+
+        g2d.setPaint(new Color(Math.abs(r.nextInt()) % 255, Math.abs(r.nextInt()) % 255, Math.abs(r.nextInt()) % 255));
+
+        for (int i = 0; i < 2000; i++) {
+            int w = Math.abs(r.nextInt()) % x;
+            int h = Math.abs(r.nextInt()) % y;
+            
+            g2d.drawLine(w, h, w, h);
+        }
 
         for (int i = 20; i <= y; i = i + 40) {
-            
-            
+
             g2d.setPaint(new Color(Math.abs(r.nextInt()) % 255, Math.abs(r.nextInt()) % 255, Math.abs(r.nextInt()) % 255));
-            
-            g2d.setStroke(new BasicStroke((r.nextInt(10-1)+1)+1, BasicStroke.CAP_BUTT,
+
+            g2d.setStroke(new BasicStroke((r.nextInt(10 - 1) + 1) + 1, BasicStroke.CAP_BUTT,
                     BasicStroke.JOIN_ROUND, 1.0f, dash, 2f));
-            
             g2d.drawLine(i, 20, i, y);
-            g2d.setStroke(new BasicStroke((r.nextInt(10-5)+1)+1, BasicStroke.CAP_BUTT,
+
+            g2d.setStroke(new BasicStroke((r.nextInt(10 - 5) + 1) + 1, BasicStroke.CAP_BUTT,
                     BasicStroke.JOIN_ROUND, 1.0f, dash, 2f));
             g2d.drawLine(20, i, y, i);
         }
@@ -52,6 +83,11 @@ class Surface extends JPanel implements Variable {
         super.paintComponent(g);
         doDrawing(g);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
 }
 
 public class Square extends JFrame implements Variable {
@@ -62,7 +98,15 @@ public class Square extends JFrame implements Variable {
     }
 
     private void initUI() {
-        add(new Surface());
+        final Surface surface = new Surface();
+        add(surface);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Timer timer = surface.getTimer();
+                timer.stop();
+            }
+        });
         setTitle("Lines and Points");
         setSize(x, y);
         setLocationRelativeTo(null);
